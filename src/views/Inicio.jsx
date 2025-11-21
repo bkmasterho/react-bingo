db085634ce233b0c58c88bb7949db85bdcafd370
Jesus Hernandez <61995628+bkmasterho@users.noreply.github.com>
2025-11-20 22:22:05 -0300

    subiendo lo demas

---
+++ src/views/Inicio.jsx
@@ -0,0 +1,68 @@
+
+export default function Inicio() {
+  return (
+      <>
+          
+          {/*<div class="mt-10 max-w-md mx-auto p-4 bg-white rounded-md shadow-md font-bold uppercase
+          text-center">
+              Compra tu carton
+          </div>*/}
+
+              <div class="mt-10 max-w-2xl mx-auto bg-white rounded-xl shadow-md overflow-hidden">
+
+                  <div class="p-4 bg-sky-500">
+                      <h2 class="text-2xl text-slate-900 font-bold uppercase text-center">Compra tu carton</h2>
+                  </div>
+
+                  <div class="m-5 font-bold text-center">
+                      ¿Cuantos cartones?
+                  </div>   
+
+                  <div class="flex gap-2 justify-center mx-auto">
+                      <button class="px-3 py-2 bg-slate-200 rounded-md hover:bg-slate-300">
+                          +
+                      </button>
+
+                      <input
+                          type="number"
+                          class="w-16 text-center border border-slate-300 rounded-md py-2"
+                      />
+
+                      <button class="px-3 py-2 bg-slate-200 rounded-md hover:bg-slate-300">
+                          -
+                      </button>
+                  </div>
+
+                  <div className="text-center mt-5">
+                        <p className="p-2">Total a pagar:</p>
+                        <span className="font-bold text-2xl">Bs 250</span>
+                  </div>
+
+                 <div class="max-w-sm mx-auto space-y-4 mb-5">
+                      <div>
+                        <label class="block mb-1 font-medium text-slate-700">Nombre</label>
+                          <input 
+                            type="text"
+                            class="w-full px-3 py-2 rounded-lg shadow-md border border-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500"
+                          />
+                        </div>
+
+                        <div>
+                          <label class="block mb-1 font-medium text-slate-700">Teléfono</label>
+                          <input 
+                            type="tel"
+                            class="w-full px-3 py-2 rounded-lg shadow-md border border-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500"
+                          />
+                        </div>
+
+                        <button 
+                          class="w-full py-3 bg-sky-500 text-white font-bold rounded-lg hover:bg-sky-600 transition">
+                          Comprar Cartones
+                        </button>
+
+
+                    </div>
+              </div>
+        </>
+  )
+}
